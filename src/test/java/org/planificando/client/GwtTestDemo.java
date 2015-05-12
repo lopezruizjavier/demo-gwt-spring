c3e7b0ed9803ac06afb85bdcf1136d7a248006a3
javier.lopezruiz <javierlopez.net@gmail.com>
2015-05-12 20:08:35 +0200

    Refactor

--- src/test/java/org/planificando/client/GwtTestDemo.java
+++ src/test/java/org/planificando/client/GwtTestDemo.java
@@ -16,13 +16,13 @@ import com.google.gwt.user.client.rpc.ServiceDefTarget;
  * See http://mojo.codehaus.org/gwt-maven-plugin/user-guide/testing.html 
  * for details.
  */
-public class GwtTestprueba extends GWTTestCase {
+public class GwtTestDemo extends GWTTestCase {
 
   /**
    * Must refer to a valid module that sources this class.
    */
   public String getModuleName() {
-    return "org.planificando.pruebaJUnit";
+    return "org.planificando.DemoJUnit";
   }
 
   /**
@@ -45,7 +45,7 @@ public class GwtTestprueba extends GWTTestCase {
     // Create the service that we will test.
     GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
     ServiceDefTarget target = (ServiceDefTarget) greetingService;
-    target.setServiceEntryPoint(GWT.getModuleBaseURL() + "prueba/greet");
+    target.setServiceEntryPoint(GWT.getModuleBaseURL() + "Demo/greet");
 
     // Since RPC calls are asynchronous, we will need to wait for a response
     // after this test method returns. This line tells the test runner to wait
